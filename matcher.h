61cb5b46e962cf7aa5a45a47d7019bb27990b098
Saige Madden <47398206+smmadden@users.noreply.github.com>
2019-12-26 15:17:58 -0800

    Update comments

--- matcher.h
+++ matcher.h
@@ -1,12 +1,11 @@
 /**
  * Functions to implement in matcher.c
- * You must not change this file.
  */
 
 #ifndef _RGREP_MATCHER_H
 #define _RGREP_MATCHER_H
 
-/* declaration of your matcher function */
+/* declaration of matcher function */
 int rgrep_matches(char *line, char *pattern);
 
 #endif
